455cd10eafb803dd922b23b582763fe84fe2fef5
Rémi.D <90192890+remi-degraeve@users.noreply.github.com>
2024-01-23 17:01:56 +0100

    puch

--- src/screens/library/Library.js
+++ src/screens/library/Library.js
@@ -46,8 +46,8 @@ const Library = () => {
 
   return (
       <div>
-          <Header icon={"cash-outline"} 
-          title={"Stockage"} 
+          <Header icon={"print-outline"} 
+          title={"Machine"} 
           position={false}
           destination = '/Machine'
           ></Header>
@@ -70,7 +70,8 @@ const Library = () => {
                       }}
                   />
                   <ProductCard
-                      product={{                          name: 'LED Verte 4V',
+                      product={{
+                          name: 'LED Verte 4V',
                           quantity: 15,
                           image: ImageBackground3,
                           setUserData
@@ -125,76 +126,127 @@ const Library = () => {
 };
 
 
+
 const ProductCard = ({ product, userData }) => {
     const [showPopup, setShowPopup] = useState(false);
     const [showPopup1, setShowPopup1] = useState(false);
-
-
+    const [inputValue, setInputValue] = useState(product.quantity);
+    const [newProductName, setNewProductName] = useState(product.name);
+    const [newProductImage, setNewProductImage] = useState(product.image);
+  
     const handleImageClick = () => {
-        // Afficher le popup si administrateur
-        if (userData && userData.admin) {
-            setShowPopup(true);
-        }else setShowPopup1(true);
+      // Afficher le popup si administrateur
+      if (userData && userData.admin) {
+        setShowPopup1(true);
+      } else setShowPopup(true);
     };
-
+  
     const onValidate = () => {
-        // Do something with the input value (in this case, log it to the console)
-        console.log('Input value:', inputValue);
-    
-        // Close the popup
+      const numericValue = parseFloat(inputValue);
+      console.log(numericValue);
+      if (!isNaN(numericValue)) {
+        console.log('Nouveau Stock :', numericValue);
         setShowPopup1(false);
+      } else {
+        alert('Valeur Fausse Entrer un Nombre');
+      }
     };
   
     const closePopup = () => {
-        setShowPopup(false);
+      setShowPopup(false);
     };
+  
     const closePopup1 = () => {
-        setShowPopup1(false);
+      setShowPopup1(false);
     };
-
-
+  
+    const handleInputChange = (event) => {
+      setInputValue(event.target.value);
+    };
+  
+    const handleNameChange = (event) => {
+      setNewProductName(event.target.value);
+    };
+  
+    const handleImageChange = (event) => {
+      const file = event.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          setNewProductImage(reader.result);
+        };
+        reader.readAsDataURL(file);
+      }
+    };
   
     return (
-        <div className="product-card">
-            <div className="product-image" onClick={handleImageClick}>
-
-                <div className="quantity-overlay">
-                    <div className="product-quantity"> Quantité disponible : {product.quantity}</div>
-                </div>
-                <img src={product.image} alt={product.name} />
-                <div className="product-name">{product.name}</div>
-            </div>
-      
-            {/* Popup */}
-            {showPopup && (
-                <div className="popup-overlay">
-                    <div className="popup" style={{ width: 'auto', height: 'auto' }}>
-                        <span className="close" onClick={closePopup}>
-                            &times;
-                        </span>
-                        <QRCode value="1234567" />
-                    </div>
-                </div>
-            )}
-            {/* Popup */}
-            {showPopup1 && (
-                <div className="popup1-overlay">
-                    <div className="popup1" style={{ width: 'auto', height: 'auto' }}>
-                        <span className="close" onClick={closePopup1}>
-                            &times;
-                        </span>
-                        <QRCode id="qrc" value="1234567" />
-
-                        <input id="adminput" type="text" placeholder="Enter something"/>
-
-                        <button id="adminbnt" onClick={onValidate}>
-                            Valider
-                        </button>
-                    </div>
-                </div>
-            )}
+      <div className="product-card">
+        <div className="product-image" onClick={handleImageClick}>
+          <div className="quantity-overlay">
+            <div className="product-quantity"> Quantité disponible: {product.quantity}</div>
+          </div>
+          <img src={product.image} alt={product.name} />
+          <div className="product-name">{product.name}</div>
         </div>
+  
+        {/* Popup */}
+        {showPopup && (
+          <div className="popup-overlay">
+            <div className="popup">
+              <span className="close" onClick={closePopup}>
+                &times;
+              </span>
+              <QRCode value="1234567" />
+            </div>
+          </div>
+        )}
+        {/* Popup */}
+        {showPopup1 && (
+            <div className="popup1-overlay">
+                <div className="popup1">
+                <span className="close" onClick={closePopup1}>
+                    &times;
+                </span>
+                <div>
+                    <label htmlFor="newProductName">Nouveau Nom:</label>
+                    <input
+                        id="newProductName"
+                        type="text"
+                        placeholder="Enter new name"
+                        value={newProductName}
+                        onChange={handleNameChange}
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="adminput">Nouvelle Quantité:</label>
+                    <input
+                        id="adminput"
+                        type="text"
+                        placeholder="Enter new quantity"
+                        value={inputValue}
+                        onChange={handleInputChange}
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="newProductImage">Nouvelle Image URL:</label>
+                    <input
+                        id="newProductImage"
+                        type="file"
+                        accept="image/*"
+                        onChange={handleImageChange}
+                        />
+                </div>
+                <button id="adminbnt" onClick={onValidate}>
+                    Valider
+                </button>
+                </div>
+            </div>
+            )}
+      </div>
     );
   };
-
-export default Library;
+  
+  export default Library;
+  
--- src/screens/machine/Machine.js
+++ src/screens/machine/Machine.js
@@ -42,8 +42,8 @@ const Machine = () => {
 
   return (
       <div>
-          <Header icon={"print-outline"} 
-          title={"Machine"} 
+          <Header icon={"cash-outline"} 
+          title={"Stockage"} 
           position={false}
           destination = '/Stockage'
           ></Header>
@@ -78,40 +78,125 @@ const Machine = () => {
 };
 
 
-const ProductCard = ({ product }) => {
-  const [showPopup, setShowPopup] = useState(false);
-
-  const handleImageClick = () => {
-      setShowPopup(true);
-  };
-
-  const closePopup = () => {
+const ProductCard = ({ product, userData }) => {
+    const [showPopup, setShowPopup] = useState(false);
+    const [showPopup1, setShowPopup1] = useState(false);
+    const [inputValue, setInputValue] = useState(product.quantity);
+    const [newProductName, setNewProductName] = useState(product.name);
+    const [newProductImage, setNewProductImage] = useState(product.image);
+  
+    const handleImageClick = () => {
+      // Afficher le popup si administrateur
+      if (userData && userData.admin) {
+        setShowPopup1(true);
+      } else setShowPopup(true);
+    };
+  
+    const onValidate = () => {
+      const numericValue = parseFloat(inputValue);
+      console.log(numericValue);
+      if (!isNaN(numericValue)) {
+        console.log('Nouveau Stock :', numericValue);
+        setShowPopup1(false);
+      } else {
+        alert('Valeur Fausse Entrer un Nombre');
+      }
+    };
+  
+    const closePopup = () => {
       setShowPopup(false);
-  };
-
-  return (
+    };
+  
+    const closePopup1 = () => {
+      setShowPopup1(false);
+    };
+  
+    const handleInputChange = (event) => {
+      setInputValue(event.target.value);
+    };
+  
+    const handleNameChange = (event) => {
+      setNewProductName(event.target.value);
+    };
+  
+    const handleImageChange = (event) => {
+      const file = event.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          setNewProductImage(reader.result);
+        };
+        reader.readAsDataURL(file);
+      }
+    };
+  
+    return (
       <div className="product-card">
-          <div className="product-image" onClick={handleImageClick}>
-              <div className="quantity-overlay">
-                  <div className="product-quantity"> Quantité disponible : {product.quantity}</div>
-              </div>
-              <img src={product.image} alt={product.name} />
-              <div className="product-name">{product.name}</div>
+        <div className="product-image" onClick={handleImageClick}>
+          <div className="quantity-overlay">
+            <div className="product-quantity"> Quantité disponible: {product.quantity}</div>
           </div>
+          <img src={product.image} alt={product.name} />
+          <div className="product-name">{product.name}</div>
+        </div>
+  
+        {/* Popup */}
+        {showPopup && (
+          <div className="popup-overlay">
+            <div className="popup">
+              <span className="close" onClick={closePopup}>
+                &times;
+              </span>
+              <QRCode value="1234567" />
+            </div>
+          </div>
+        )}
+        {/* Popup */}
+        {showPopup1 && (
+            <div className="popup1-overlay">
+                <div className="popup1">
+                <span className="close" onClick={closePopup1}>
+                    &times;
+                </span>
+                <div>
+                    <label htmlFor="newProductName">Nouveau Nom:</label>
+                    <input
+                        id="newProductName"
+                        type="text"
+                        placeholder="Enter new name"
+                        value={newProductName}
+                        onChange={handleNameChange}
+                    />
+                </div>
 
-          {/* Popup */}
-          {showPopup && (
-              <div className="popup-overlay">
-                  <div className="popup" style={{ width: 'auto', height: 'auto' }}>
-                      <span className="close" onClick={closePopup}>
-                          &times;
-                      </span>
-                      <QRCode value="1234567" />
-                  </div>
-              </div>
-          )}
+                <div>
+                    <label htmlFor="adminput">Nouvelle Quantité:</label>
+                    <input
+                        id="adminput"
+                        type="text"
+                        placeholder="Enter new quantity"
+                        value={inputValue}
+                        onChange={handleInputChange}
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="newProductImage">Nouvelle Image URL:</label>
+                    <input
+                        id="newProductImage"
+                        type="file"
+                        accept="image/*"
+                        onChange={handleImageChange}
+                        />
+                </div>
+                <button id="adminbnt" onClick={onValidate}>
+                    Valider
+                </button>
+                </div>
+            </div>
+            )}
       </div>
-  );
-};
+    );
+  };
 
 export default Machine;
