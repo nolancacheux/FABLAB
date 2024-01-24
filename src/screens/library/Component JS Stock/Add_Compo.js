3e6db9819ce4aa7abee1e9c7cb76f77ab9feaf4d
Hadrien62 <hadrien.delobel@student.junia.com>
2024-01-24 18:09:19 +0100

    matthieu

--- src/screens/library/Component JS Stock/Add_Compo.js
+++ src/screens/library/Component JS Stock/Add_Compo.js
@@ -1,6 +1,44 @@
 import React, {useState} from "react";
+import axios from "axios";
 import {handleImageChange, handleImageClick, handleInputChange, handleNameChange, onValidate} from "../Function";
 import ImageAdd from "../Images Compo/addsymbole.png";
+
+const ImageUploader = () => {
+    const [selectedFile, setSelectedFile] = useState(null);
+  
+    const handleFileChange = (event) => {
+      setSelectedFile(event.target.files[0]);
+    };
+  
+    const handleUpload = async () => {
+      if (!selectedFile) {
+        console.error("Veuillez sélectionner un fichier.");
+        return;
+      }
+  
+      const formData = new FormData();
+      formData.append("image", selectedFile);
+  
+      try {
+        const response = await axios.post("https://192.168.184.122:1234/stock/upload", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        });
+  
+        console.log("Réponse du serveur:", response.data);
+      } catch (error) {
+        console.error("Erreur lors de l'envoi de l'image:", error.message);
+      }
+    };
+  
+    return (
+      <div>
+        <input type="file" onChange={handleFileChange} />
+        <button onClick={handleUpload}>Envoyer l'image</button>
+      </div>
+    );
+  };
 
 const Add_Compo = ({admin}) => {
     const [showPopup, setShowPopup] = useState(false);
@@ -40,12 +78,7 @@ const Add_Compo = ({admin}) => {
                     </div>
                     <div>
                         <label htmlFor="newProductImage">Nouvelle Image URL:</label>
-                        <input
-                            id="newProductImage"
-                            type="file"
-                            accept="image/*"
-                            onChange={handleImageChange}
-                        />
+                        <ImageUploader />
                     </div>
                     <button id="adminbnt" onClick={onValidate(showPopup1)}>
                             Valider
