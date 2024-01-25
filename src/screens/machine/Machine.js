bbeabb3db89f4fe39e1a57d8b4cb9d1ccd28577f
Nolan CACHEUX <nolan.cacheux@student.junia.com>
2024-01-25 17:37:06 +0100

    x

--- src/screens/machine/Machine.js
+++ src/screens/machine/Machine.js
@@ -12,6 +12,13 @@ import Add_Compo_Machine from "./Component JS Stock/Add_Compo_Machine.js";
 import perceuse from "../../assets/images/perceuse.png";
 import tournevis from "../../assets/images/tournevis.png";
 import marteau from "../../assets/images/marteau.png";
+import Decoupe from "../../equipements/decoupeuselaser.jpeg"
+import Scanner from "../../equipements/scanner3D.jpg"
+import Impr from "../../equipements/imprimante3D.jpg"
+import sublim from "../../equipements/sublimation.png"
+import Poste from "../../equipements/posteelectronique.jpg"
+import Plotteur from "../../equipements/plotteurdecoupe.jpg"
+import Fraiseuse from "../../equipements/fraiseusecnc.png"
 
 import axios from "axios";
 
@@ -32,7 +39,7 @@ const ProduitList = (admin) => {
             }
         };
 
-        fetchProduits2(); 
+        fetchProduits2();
     }, []);
 
 
@@ -48,7 +55,7 @@ const ProduitList = (admin) => {
             }
         };
 
-        fetchProduits3();   
+        fetchProduits3();
     }, []);
 
     return (
@@ -57,73 +64,129 @@ const ProduitList = (admin) => {
             {loading ? (
                 <p>Chargement en cours...</p>
             ) : (
-                
+
                 <div className="product-container">
+                    <Product_Card_Machine product={{
+                        name: "Découpeuse laser",
+                        pret: "2024-01-20",
+                        nbJour: 3,
+                        reserved: false,
+                        is_late: false,
+                        image: Decoupe
+                    }} />
+                    <Product_Card_Machine product={{
+                        name: "Scanner 3D",
+                        pret: "2024-01-20",
+                        nbJour: 3,
+                        reserved: false,
+                        is_late: false,
+                        image: Scanner
+                    }} />
+                    <Product_Card_Machine product={{
+                        name: "Poste à souder",
+                        pret: "2024-01-20",
+                        nbJour: 3,
+                        reserved: false,
+                        is_late: false,
+                        image: Poste
+                    }} />
+                    <Product_Card_Machine product={{
+                        name: "Fraiseuse",
+                        pret: "2024-01-20",
+                        nbJour: 3,
+                        reserved: false,
+                        is_late: false,
+                        image: Fraiseuse
+                    }} />
+                    <Product_Card_Machine product={{
+                        name: "Plotteur de coupe",
+                        pret: "2024-01-20",
+                        nbJour: 3,
+                        reserved: false,
+                        is_late: false,
+                        image: Plotteur
+                    }} />
+                    <Product_Card_Machine product={{
+                        name: "Sublimation",
+                        pret: "2024-01-20",
+                        nbJour: 3,
+                        reserved: false,
+                        is_late: false,
+                        image: sublim
+                    }} />
+                    <Product_Card_Machine product={{
+                        name: "Imprimante 3D",
+                        pret: "2024-01-20",
+                        nbJour: 3,
+                        reserved: false,
+                        is_late: false,
+                        image: Impr
+                    }} />
                     <Product_Card_Outils key={1}
-                                product={{
-                                    name: "Perceuse sans fil",
-                                    pret: "2024-01-20",
-                                    nbJour: 3,
-                                    reserved: false,
-                                    is_late: false,
-                                    image: perceuse,  
-                                }}
-                                admin={admin}
-                        />
-                        <Product_Card_Outils key={2}
-                                product={{
-                                    name: "Tournevis",
-                                    pret: "2024-01-20",
-                                    nbJour: 3,
-                                    reserved: false,
-                                    is_late: false,
-                                    image: tournevis
-                                }}
-                                admin={admin}
-                        />
-                        <Product_Card_Outils key={3}
-                                product={{
-                                    name: "Marteau",
-                                    pret: "2024-01-20",
-                                    nbJour: 3,
-                                    reserved: false,
-                                    is_late: false,
-                                    image: marteau,  
-                                }}
-                                admin={admin}
-                        />
-            
+                        product={{
+                            name: "Perceuse sans fil",
+                            pret: "2024-01-20",
+                            nbJour: 3,
+                            reserved: false,
+                            is_late: false,
+                            image: perceuse,
+                        }}
+                        admin={admin}
+                    />
+                    <Product_Card_Outils key={2}
+                        product={{
+                            name: "Tournevis",
+                            pret: "2024-01-20",
+                            nbJour: 3,
+                            reserved: false,
+                            is_late: false,
+                            image: tournevis
+                        }}
+                        admin={admin}
+                    />
+                    <Product_Card_Outils key={3}
+                        product={{
+                            name: "Marteau",
+                            pret: "2024-01-20",
+                            nbJour: 3,
+                            reserved: false,
+                            is_late: false,
+                            image: marteau,
+                        }}
+                        admin={admin}
+                    />
+
                     {produits2.map((produit) => (
                         <Product_Card_Outils key={produit._id}
-                                product={{
-                                    name: produit.name,
-                                    pret: produit.pret,
-                                    nbJour: produit.nbJour,
-                                    reserved: produit.reserved,
-                                    is_late: produit.is_late,
-                                    image: `https://${config.ipserveur}:${config.portserveur}/uploads/${produit.image1}`,  // Utilisez le préfixe /uploads
-                                }}
-                                admin={admin}
+                            product={{
+                                name: produit.name,
+                                pret: produit.pret,
+                                nbJour: produit.nbJour,
+                                reserved: produit.reserved,
+                                is_late: produit.is_late,
+                                image: `https://${config.ipserveur}:${config.portserveur}/uploads/${produit.image1}`,  // Utilisez le préfixe /uploads
+                            }}
+                            admin={admin}
                         />
                     ))}
                     {produits3.map((produit) => (
                         <Product_Card_Machine key={produit._id}
-                                product={{
-                                    name: produit.name,
-                                    pret: produit.pret,
-                                    nbHeure: produit.nbJour,
-                                    reserved: produit.reserved,
-                                    is_late: produit.is_late,
-                                    image: `https://${config.ipserveur}:${config.portserveur}/uploads/${produit.image1}`,  // Utilisez le préfixe /uploads
-                                }}
-                                admin={admin}
+                            product={{
+                                name: produit.name,
+                                pret: produit.pret,
+                                nbHeure: produit.nbJour,
+                                reserved: produit.reserved,
+                                is_late: produit.is_late,
+                                image: `https://${config.ipserveur}:${config.portserveur}/uploads/${produit.image1}`,  // Utilisez le préfixe /uploads
+                            }}
+                            admin={admin}
                         />
                     ))}
                     {admin && (
-                        <Add_Compo_Machine admin={admin}/>
+                        <Add_Compo_Machine admin={admin} />
                     )}
                     {admin && (
-                        <Add_Compo_Outils admin={admin}/>
+                        <Add_Compo_Outils admin={admin} />
                     )}
                 </div>
             )}
@@ -132,30 +195,31 @@ const ProduitList = (admin) => {
 };
 
 const Machine = () => {
-     
 
-  let admin=false;
-  return (
-      <div>
-          <Header icon={"cash-outline"} 
-          title={"Stockage"} 
-          position={false}
-          destination = '/Stockage'
-          ></Header>
-          <section>
-              <ProduitList admin={admin}/>
-              
-          </section>
-          <Navigation
-              library={true}
-              search={false}
-              map={false}
-              profil={false}
-              setting={false}
-              position={false}
-          />
-      </div>
-  );
+
+    let admin = false;
+    return (
+
+        <div>
+            <Header icon={"cash-outline"}
+                title={"Stockage"}
+                position={false}
+                destination='/Stockage'
+            ></Header>
+            <section>
+                <ProduitList admin={admin} />
+
+            </section>
+            <Navigation
+                library={true}
+                search={false}
+                map={false}
+                profil={false}
+                setting={false}
+                position={false}
+            />
+        </div>
+    );
 };
-  
+
 export default Machine;
